f0ab5160cd1e9e111b762cf498fd85cb2bc05699
Romain <romainsorrnti@gmail.com>
2020-05-04 10:14:52 +0200

    Adding fr page

---
+++ src/pages/fr.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/Layout/Layout"
+
+import "../components/css/index.css"
+
+const FrPage = () => {
+  return (
+    <React.Fragment>
+      <Layout />
+    </React.Fragment>
+  )
+}
+
+export default FrPage
