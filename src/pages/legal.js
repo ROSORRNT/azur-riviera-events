08ad7e0040296167a5052839b34d7e9162a59f6b
Romain <romainsorrnti@gmail.com>
2020-05-12 09:14:30 +0200

    Adding legal page

--- src/pages/legal.js
+++ src/pages/legal.js
@@ -1,7 +1,145 @@
 import React from "react"
 
+import Card from "../components/UIElements/Card"
+import MainNavigation from "../components/Navigation/MainNavigation"
+
 const legal = () => {
-  return <div>Mentions Légales</div>
+  return (
+    <div>
+      <MainNavigation />
+      <Card>
+        <div style={{ textAlign: "center" }}>
+          <h1>Mentions Légales</h1>
+          <p>Copyright Azur Riviera Events 2020®</p>
+          <p>
+            <span>N° immatriculation ATOUT FRANCE : IM006140002 –</span> <br />
+            <span>
+              GROUPAMA CAUTION 5 rue du Centre, 93199 NOISY LE GRAND CEDEX –
+            </span>{" "}
+            <br />
+            <span>RCP HISCOX 19 Rue Louis Le Grand, 75002 PARIS –</span> <br />
+            <span>N° d’immatriculation CNIL : 1772613 v 0</span>
+          </p>
+          <p>
+            Le site web dénommé " Azur Riviera Events" est édité par la société
+            AZUR RIVIERA PRESTATIONS, SARL, dont le siège social est situé à :
+            COLOMARS (06670), 45 Chemin Collet Baraya, immatriculée au registre
+            du commerce et des sociétés de NICE sous le numéro 484 003 306.
+          </p>
+          <p>
+            Responsable de la publication : Patricia LE BRAS Site réalisé par :
+            AZUR RIVIERA PRESTATIONS
+          </p>
+          <h3>
+            {" "}
+            <strong>
+              {" "}
+              Respect de la vie privée et des données personnelles
+            </strong>
+          </h3>
+          <p>
+            Droit d'accès et de modification des données concernant nos
+            visiteurs. Politique de respect de la vie privée adoptée par AZUR
+            RIVIERA PRESTATIONS : Vous disposez d'un droit d'accès, de
+            modification, de rectification et de suppression des données qui
+            vous concernent, s'il s'en trouvait (art.34 de la loi "Informatique
+            et Libertés" nº 78-17 du 16 janvier 1978).
+          </p>
+          <h3>Utilisation des cookies</h3>
+          <p>
+            AZUR RIVIERA PRESTATIONS peut être amené à utiliser le système des
+            "cookies". Le cookie ne nous permet pas de vous identifier mais de
+            manière générale de collecter des informations lors de votre passage
+            sur notre site. Cela nous permettra de savoir quelles sont les
+            parties de notre site qui vous intéressent.
+          </p>
+          <p>
+            AZUR RIVIERA PRESTATIONS pourra alors mieux connaître vos centres
+            d'intérêt et vous communiquer des informations adaptées à vos
+            besoins. Ces "cookies" vous éviteront également de devoir fournir
+            chaque fois des informations que vous nous avez déjà communiquées
+            dans la mesure où ils se souviendront que vous nous avez communiqué
+            ces informations à une date antérieure. Il vous est possible de
+            détecter l'existence de ces "cookies" et, le cas échéant, de les
+            supprimer. Il vous est possible de vous opposer à l'enregistrement
+            de cookies en désactivant cette fonction de votre navigateur dans
+            les préférences de celui-ci.
+          </p>
+          <h3>Sécurité et confidentialité sur le réseau</h3>
+          <p>
+            Les messages que vous nous ferez parvenir par l'intermédiaire
+            d'Internet peuvent être interceptés sur le réseau. Jusqu'à ce qu'ils
+            nous parviennent, leur confidentialité ne peut être garantie.
+            Assurez-vous de ne pas divulguer d'informations personnelles ou
+            confidentielles inutiles, sensibles ou provenant de tiers. De plus,
+            l'indication de la provenance des messages électroniques que nous
+            recevons peut être falsifiée. Par conséquent, si vous souhaitez nous
+            communiquer de telles informations, utilisez impérativement la voie
+            postale. En conséquence, aucune plainte, déclaration ou demande de
+            conseil transmise par message Internet ne sera prise en compte.
+            Veuillez formuler vos demandes exclusivement par voie postale. Pour
+            obtenir plus de renseignements sur les procédures à suivre ou sur
+            des dossiers en cours d'instruction, veuillez prendre contact avec
+            AZUR RIVIERA PRESTATIONS par écrit.
+          </p>
+          <h3>Respect des droits d'auteur</h3>
+          <p>Utilisation des informations du site et droits d'auteur.</p>
+          <p>
+            L'utilisation de tout document (photos, textes, images ...)
+            provenant du site n'est autorisée qu'à titre d'information pour un
+            usage privé. Toute utilisation qui pourrait être effectuée à des
+            fins commerciale est interdite. AZUR RIVIERA PRESTATIONS s'efforce
+            d'assurer au mieux de ses possibilités, l'exactitude et la mise à
+            jour des informations diffusées sur internet. Toutefois nous
+            insistons sur le fait qu'il appartient aux visiteurs de vérifier les
+            informations présentes sur le site, y compris en nous contactant. En
+            conséquence, AZUR RIVIERA PRESTATIONS décline toute responsabilité :
+            pour toute imprécision, inexactitude ou omission portant sur des
+            informations disponibles sur le site ; pour tous dommages résultant
+            d'une intrusion frauduleuse d'un tiers ayant entraîné une
+            modification des informations mises à la disposition sur le site ;
+            et plus généralement pour tous dommages, directs ou indirects,
+            qu'elles qu'en soient les causes, origines, nature ou conséquences,
+            provoqués à raison de l'accès de quiconque au site ou de
+            l'impossibilité d'y accéder, de même que l'utilisation du site et/ou
+            du crédit accordé à une quelconque information provenant directement
+            ou indirectement de ce dernier.
+          </p>
+          <p>
+            Par ailleurs, sauf mention contraire, les droits de propriété
+            intellectuelle sur les documents contenus dans le site et chacun des
+            éléments créés pour le site sont la propriété exclusive d' AZUR
+            RIVIERA PRESTATIONS ou de ses fournisseurs, ceux-ci ne concédant
+            aucune licence, ni aucun droit que celui de consulter le site. La
+            reproduction de tous documents publiés sur le site est seulement
+            autorisée aux fins exclusives d'informations pour un usage personnel
+            et privé, toute reproduction et toute utilisation de copies
+            réalisées à d'autres fins étant expressément interdite. Toutes les
+            marques citées dans ce site appartiennent à leurs compagnies
+            respectives. Tous les produits, logos, et images cités dans les
+            pages de ce site sont la propriété de leur marque respective.
+          </p>
+          <h3>Informations d'ordre technique</h3>
+          <p>
+            Le présent site est accessible 24 heures sur 24 et 7 jours sur 7, à
+            l'exception des cas de force majeure, difficultés informatiques,
+            maintenance, difficultés liées à la structure des réseaux de
+            communication ou difficultés d'ordre technique.
+          </p>
+          <h3>Application de la loi Française</h3>
+          <p>
+            Le non-respect des présentes dispositions expose le contrevenant à
+            des poursuites et à des sanctions pénales. Le présent document est
+            régi et interprété selon le droit Français et relève de la
+            compétence exclusive des Tribunaux Français. Si l'une des
+            dispositions du présent texte s'avérait nulle, non valide ou sans
+            effet juridique, toutes les autres dispositions demeureraient
+            applicables.
+          </p>
+        </div>
+      </Card>
+    </div>
+  )
 }
 
 export default legal
